51cb5147735911b9045839b311b03198f6beb66b
rahulbabu7 <rahulbabu436@gmail.com>
2023-09-16 20:16:43 +0530

    added fading animation when it arrives

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+const showtext = ref(false); 
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Transition name="fade">
+
+      <h1 v-if="showtext">HelloWorld</h1> 
+    </Transition>
+
+ <button @click="showtext = !showtext"> Toggle </button>
   </main>
 </template>
+<style scoped>
+
+.fade-enter-from{
+  /* background-color: yellow; */
+  opacity: 0;
+}
+
+.fade-enter-to{
+  /* background-color: blue; */
+  opacity: 1;
+}
+.fade-enter-active{
+  transition: all 5s ease;
+
+}
+</style>
