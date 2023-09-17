8de4f179a439a6a2ddbe80c2761679bd6827fdbe
rahulbabu7 <rahulbabu436@gmail.com>
2023-09-17 15:16:57 +0530

    add

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,34 @@
 <script setup>
 import { ref } from "vue";
-const showtext = ref(true);
+// const showtext = ref(true);
+const textInput = ref("");
+const names = ref(['sunny','amal'])
 </script>
 
 <template>
   <main>
-    <section>
+    <!-- <section>
       <Transition name="fade">
         <h1 v-if="showtext">HelloWorld</h1>
         <h1 v-else>BIE</h1>
       </Transition>
     </section>
-    <button @click="showtext = !showtext">Toggle</button>
+    <button @click="showtext = !showtext">Toggle</button> -->
+    <section>
+      <input type="text" placeholder="Enter the name" v-model="textInput">
+      <ul v-for="name in names">
+      
+        <li>
+         {{ name }}
+        </li>
+        
+      </ul>
+    </section>
   </main>
 </template>
 <style scoped>
-.fade-enter-from {
-  /* background-color: yellow; */
+/* .fade-enter-from {
+  /* background-color: yellow; 
   opacity: 0;
 }
 
@@ -24,7 +36,7 @@ const showtext = ref(true);
 .fade-enter-to{
   /* background-color: blue; 
   opacity: 1;
-} */
+} 
 .fade-enter-active {
   transition: all 5s ease;
 }
@@ -32,7 +44,7 @@ const showtext = ref(true);
 .fade-leave-from{
   opacity: 1;
 
-} */
+} 
 .fade-leave-to {
   opacity: 0;
 }
@@ -47,5 +59,35 @@ h1{
 }
 button{
   margin-top: 50px;
+} */
+
+section{
+  max-width: 300px;
+  margin: 0 auto;
+}
+input{
+  width: 100%;
+  border-radius: 5px;
+  border: 1px solid rebeccapurple;
+  padding: 10px;
+  margin-bottom: 20px;
+  box-shadow: 1px 1px 10px rgba(0,0,0,0.12);
+}
+ul{
+  display: inline;
+  list-style: none;
+  margin: 0px;
+  padding: 0px;
+}
+li{
+  list-style: none;
+  border-radius: 5px;
+  width: 300px;
+  padding: 5px 10px;
+  margin-top: 10px;
+  box-shadow: 1px 1px 10px rgba(0,0,0,0.12);
+  text-align: center;
+  cursor: pointer;
+
 }
 </style>
